214a01810f4f43e43c8bb72e22986f54091df5a8
yurkorudi <48592176+yurkorudi@users.noreply.github.com>
2024-10-12 01:46:38 +0300

    ..
    ..

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    return render_template('homepage.html')
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)

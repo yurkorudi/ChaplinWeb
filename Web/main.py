294e67ddda35833ea01470c57a1f61d8ced7cf71
yurkorudi <48592176+yurkorudi@users.noreply.github.com>
2024-10-15 23:53:08 +0300

    movies added
    @PaulStefanyshyn please dont throught it down, commit in common file))

--- Web/main.py
+++ Web/main.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    return render_template('index.html')
+    return render_template('Homepage.html')
+
+@app.route('/movies')
+def movies():
+    return render_template('Movies.html')
 
 if __name__ == '__main__':
     app.run(debug=True) 

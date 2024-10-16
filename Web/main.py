89ae6d6c2ae602198f5862b20cc62ed335d08990
yurkorudi <48592176+yurkorudi@users.noreply.github.com>
2024-10-16 10:58:39 +0300

    buttons effect

--- Web/main.py
+++ Web/main.py
@@ -6,9 +6,13 @@ app = Flask(__name__)
 def homepage():
     return render_template('Homepage.html')
 
-@app.route('/movies')
+@app.route('/Movies.html')
 def movies():
     return render_template('Movies.html')
+
+@app.route('/About.html')
+def about():
+    return render_template('About.html')
 
 if __name__ == '__main__':
     app.run(debug=True) 

7012a5e35bb20016574a15b352ec52382c392874
yurkorudi <48592176+yurkorudi@users.noreply.github.com>
2024-10-28 13:38:32 +0200

    logic update
    NOT WORKING

--- Web/main.py
+++ Web/main.py
@@ -8,42 +8,48 @@ cities = {
     "dol":"Долина",
     "oks":"Оксана",
 }
+user_location = []
+
+
+def location():
+    global user_location
+    global cities
+    if user_location == []:
+        print("===", request.args)
+        location = request.args.get('location')
+        city = ""
+        if location:
+            city = cities[location]
+            print ("----- LocatioN: " + str(location))
+            user_location.append(city)
+            user_location.append(cities)
+
+        print ("----- >>", user_location)
+    return 
+
 
 
 @app.route('/home')
-def location():
-    print("===", request.args)
-    location = request.args.get('location')
-    city = ""
-    if location:
-        city = cities[location]
-    print ("----- LocatioN: " + str(location))
-
-    return render_template('Homepage.html', city=city, cities=cities)
-
-@app.route()
 def homepage():
-    print("===", request.args)
-    location = request.args.get('location')
-    city = ""
-    if location:
-        city = cities[location]
-    print ("----- LocatioN: " + str(location))
-
-    return render_template('Homepage.html', city=city, cities=cities)
+    global user_location
+    global cities
+    if user_location == []:
+        a = location()
+        user_location.append(a[0])
+        user_location.append(a[1])
+        return render_template('Homepage.html', city=user_location[0], cities=user_location[1])
+    else:
+        return render_template('Homepage.html', city = "", cities = cities)
 
 
 
 
 @app.route('/movies')
 def movies():
-    location = request.args.get('location')
-    city = ""
-    if location:
-        city = cities[location]
-
-    print ("----- LocatioN: " + str(location))
-    return render_template('Movies.html', city=city, cities=cities)
+    global user_location
+    if user_location == []:
+        a = location()
+    return render_template('Movies.html', city=user_location[0], cities=user_location[1])
 
 
 

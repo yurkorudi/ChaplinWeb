5bc743d6c48b32283a8869365939c54ea299b371
yurkorudi <48592176+yurkorudi@users.noreply.github.com>
2024-11-07 12:56:40 +0200

    not ready
    > to use file mdb
    > continue in modls
    >connect to main.py
    > get city and film info frop html's
    >provide film info for html's

--- Web/modls.py
+++ Web/modls.py
@@ -1,3 +1,6 @@
+from werkzeug.datastructures import ImmutableMultiDict
+
+
 class JSN: 
     def __init__(self, json, filepath, type):
         self.filepath = filepath
@@ -6,8 +9,33 @@ class JSN:
         print ("####################################################LALALAND##############################################################")
         with open (self.filepath, 'r') as self.file:
             self.dirty_data = json.load(self.file)
-            print (self.dirty_data)
             self.data = json.dumps(self.dirty_data, indent=4)
-            print (self.data)
+            print ("####################################################LALALAND##############################################################")
+            self.cities = list(self.dirty_data['location'].keys())
+            print (self.cities)
+
+
+
+    def city (self, cityname):
+        self.cityname = cityname
+        if self.cityname in self.cities:
+            self.CITY = self.cityname
+        else:
+            return
+        print (self.CITY)
+        self.films = list(self.dirty_data['location'][self.CITY]['films'].keys())
+        print (self.films)
+        return self.films
+    
+
+    def film (self, filmname):
+        self.filmname = filmname
+        if self.filmname in self.films:
+            self.FILM = self.filmname
+        else:
+            return
+        self.filmdict = self.dirty_data['location'][self.CITY]['films'][self.FILM]
+
+        print (self.filmdict)
 
 

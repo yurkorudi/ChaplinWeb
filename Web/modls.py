98c5f92d4ecef3ef0afa06ce17a3f7c56b9f1af9
PaulStefanyshyn <126797697+PaulStefanyshyn@users.noreply.github.com>
2024-12-17 14:45:29 +0200

    impruve datetime transfer

--- Web/modls.py
+++ Web/modls.py
@@ -9,7 +9,7 @@ class Film_obj:
         self.session = get_sessions(film_id=self.data['film_id'])
 
         for g in self.session:
-            self.film_dates.append(g['session_datetime'])
+            self.film_dates.append(g['session_datetime'].strftime("%Y-%m-%d %H:%M:%S"))
             #self.busy_seats[g['session_id']] = g['']#Доробити: film_dates працює добре, потрібно лиш передати 
             #словником відповідну ід сесії дату і зробити busy_seats словником в якому передаються всі 
             # заброньовані місця відповідно до певного ід
@@ -19,15 +19,17 @@ class Film_obj:
                 if id not in self.busy_seats:
                     self.busy_seats[id] = []
                 self.busy_seats[id].append(s['seat_id'])
-                print ("___________________________________________________________________")
-                print(self.busy_seats[id])
-                print ("___________________________________________________________________")
-        
+            print ("___________________________________________________________________")
+            print(g['session_datetime'])
+            print ("___________________________________________________________________")
         src = get_images(id=self.data['image_id'])
 
         path = src['path']
         
-        #date_time = self.session['session_datetime']
+        date_time = ",".join(self.film_dates)
+        print ("___________________________________________________________________")
+        print(date_time)
+        print ("___________________________________________________________________")
 
         self.data.update({'img_src': path})
         self.data.update({'date_time': self.film_dates})
